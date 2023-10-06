7fceda70a58ead9de7d6ba951c72789b73af5e44
hvvvvvv- <1264178545@qq.com>
2023-10-06 22:51:30 +0800

    add scrollview of index page

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,11 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="home">
+		<scroll-view scroll-x="true" class="navscroll">
+			<view class="item" v-for="item in 10">国内</view>
+		</scroll-view>
+		
+		<view class="content">
+			<div class="row">每一行的新闻</div>
 		</view>
 	</view>
 </template>
@@ -23,30 +26,27 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss" scoped>
+	
+.navscroll{
+	height: 100rpx;
+	white-space: nowrap;
+	background: #F7FBFA;
+	/deep/ ::-webkit-scrollbar{
+		width: 4px !important;
+		height: 1px !important;
+		overflow: auto !important;
+		background: transparent !important;
+		-webkit-appearance: auto !important;
+		display: block;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.item{
+		font-size: 40rpx;
+		display: inline-block;
+		padding: 0 30rpx;
+		line-height: 100rpx;
+		color: #333;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
+}
 </style>
