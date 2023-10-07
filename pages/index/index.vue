cc35c7038ba86473b14a40ebe26727c09428d0d4
hvvvvvv- <1264178545@qq.com>
2023-10-07 23:35:45 +0800

    style of index & user page

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,14 @@
 <template>
 	<view class="home">
 		<scroll-view scroll-x="true" class="navscroll">
-			<view class="item" v-for="item in 10">国内</view>
+			<view class="item" :class="navIndex===index?'active':''" v-for="(item, index) in 10"
+				@click="clickNav(index)">国内</view>
 		</scroll-view>
-		
+
 		<view class="content">
-			<div class="row">每一行的新闻</div>
+			<div class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</div>
 		</view>
 	</view>
 </template>
@@ -14,39 +17,59 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				navIndex: 0,
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+		clickNav(index){
+			this.navIndex = index
+		}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	
-.navscroll{
-	height: 100rpx;
-	white-space: nowrap;
-	background: #F7FBFA;
-	/deep/ ::-webkit-scrollbar{
-		width: 4px !important;
-		height: 1px !important;
-		overflow: auto !important;
-		background: transparent !important;
-		-webkit-appearance: auto !important;
-		display: block;
+	.navscroll {
+		height: 100rpx;
+		white-space: nowrap;
+		background: #F7FBFA;
+		position: fixed;
+		top: var(--window-top);
+		left: 0;
+		z-index: 10;
+
+		/deep/ ::-webkit-scrollbar {
+			width: 4px !important;
+			height: 1px !important;
+			overflow: auto !important;
+			background: transparent !important;
+			-webkit-appearance: auto !important;
+			display: block;
+		}
+
+		.item {
+			font-size: 40rpx;
+			display: inline-block;
+			padding: 0 30rpx;
+			line-height: 100rpx;
+			color: #333;
+			&.active{
+				color: #31c27c;
+			}
+		}
+
 	}
-	.item{
-		font-size: 40rpx;
-		display: inline-block;
-		padding: 0 30rpx;
-		line-height: 100rpx;
-		color: #333;
+
+	.content {
+		padding: 30rpx;
+		padding-top: 100rpx;
+
+		.row {
+			border-bottom: 1px dotted #efefef;
+			padding: 20rpx 0;
+		}
 	}
-	
-}
 </style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,14 @@
 <template>
-	<view>
-		
+	<view class="user">
+		<view class="top">
+			<image src="../../static/images/history.png" mode=""></image>
+			<text>浏览历史</text>
+		</view>
+		<view class="content">
+			<view class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -15,5 +23,31 @@
 </script>
 
 <style lang="scss">
-
+.user{
+	.top{
+		padding: 50rpx 0;
+		background: #f8f8f8;
+		color: #666;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		image{
+			width: 150rpx;
+			height: 150rpx;
+		}
+		text{
+			font-size: 38rpx;
+			padding-top: 20rpx;
+		}
+	}
+		
+	.content{
+		padding: 30rpx;
+		.row{
+			border-bottom: 1px dotted #efefef;
+			padding: 20rpx 0;
+		}
+	}
+}
 </style>
